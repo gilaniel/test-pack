51903df4edf89b67a23b08833dda9f1e809aa8f2
gila <gila@kedoo.com>
2022-05-27 14:29:45 +0300

    up

--- theme/components/text.ts
+++ theme/components/text.ts
@@ -1,9 +1,38 @@
 export default {
   sizes: {
+    head1: {
+      fontSize: "head1",
+      lineHeight: "head1",
+      fontWeight: 500,
+    },
+    head2: {
+      fontSize: "head2",
+      lineHeight: "head2",
+      fontWeight: 500,
+    },
+    head3: {
+      fontSize: "head3",
+      lineHeight: "head3",
+      fontWeight: 500,
+    },
+    head4: {
+      fontSize: "head4",
+      lineHeight: "head4",
+      fontWeight: 500,
+    },
+    head5: {
+      fontSize: "head5",
+      lineHeight: "head5",
+      fontWeight: 500,
+    },
+    head6: {
+      fontSize: "head6",
+      lineHeight: "head6",
+      fontWeight: 500,
+    },
     sub1: {
       fontSize: "sub1",
       lineHeight: "sub1",
-      fontWeight: "semibold",
     },
     sub2: {
       fontSize: "sub2",
@@ -14,24 +43,6 @@ export default {
       fontSize: "sub3",
       lineHeight: "sub3",
       fontWeight: "semibold",
-    },
-    sub4: {
-      fontSize: "cap2",
-      lineHeight: "cap2",
-      fontWeight: "semibold",
-    },
-    cap1: {
-      fontSize: "cap1",
-      lineHeight: "cap1",
-      fontWeight: "medium",
-    },
-    cap2: {
-      fontSize: "cap2",
-      lineHeight: "cap2",
-    },
-    cap3: {
-      fontSize: "cap3",
-      lineHeight: "cap3",
     },
     body1: {
       fontSize: "body1",
