9b15d291d01039f77b8231346ba4fe491461ad38
gila <gila@kedoo.com>
2022-06-20 18:40:31 +0300

    fix footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import { Flex, Link, List, ListItem } from "@chakra-ui/react";
-import { useIntl } from "react-intl";
-
-import footerPages from "../../content/footerPages";
 
 import { Socials } from "../";
 import { FooterProps } from "./Footer.types";
 
-export const Footer: React.FC<FooterProps> = () => {
-  const { formatMessage } = useIntl();
-
+export const Footer: React.FC<FooterProps> = ({ pages }) => {
   return (
     <Flex px={8} py={8} justifyContent="space-between" alignItems="center">
       <List display="flex" fontSize="12px">
-        {footerPages.map((item, i) => (
+        {pages.map((item, i) => (
           <ListItem key={i} color="gray.100" mr={6}>
-            <Link href={item.href}>{formatMessage(item)}</Link>
+            <Link href={item.href}>{item.title}</Link>
           </ListItem>
         ))}
       </List>
--- components/Footer/Footer.types.ts
+++ components/Footer/Footer.types.ts
@@ -1,3 +1,12 @@
 import type { HTMLChakraProps } from "@chakra-ui/react";
 
-export interface FooterProps extends HTMLChakraProps<"div"> {}
+export interface FooterProps extends HTMLChakraProps<"div"> {
+  pages: [
+    {
+      id: string;
+      defaultMessage: string;
+      href: string;
+      title: string;
+    }
+  ];
+}
