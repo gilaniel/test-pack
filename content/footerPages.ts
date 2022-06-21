4cafe56cd93bda3d8addb22a3b74f0aca5b06bc5
gila <gila@kedoo.com>
2022-06-21 17:14:39 +0300

    add footer links

--- content/footerPages.ts
+++ content/footerPages.ts
@@ -1,8 +1,20 @@
 export default [
   {
     id: "footer.pages.terms",
-    defaultMessage: "Terms",
+    defaultMessage: "Terms and conditions",
     href: "https://duqqy.com/terms",
+    title: "",
+  },
+  {
+    id: "footer.pages.policy",
+    defaultMessage: "Privacy policy",
+    href: "https://duqqy.com/policy/privacy",
+    title: "",
+  },
+  {
+    id: "footer.pages.cookie",
+    defaultMessage: "Cookies policy",
+    href: "https://duqqy.com/policy/cookie",
     title: "",
   },
 ];
