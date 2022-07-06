71fbdb516ad1b33f4bc44a2f270dc749669c59d6
gila <gila@kedoo.com>
2022-07-06 16:29:15 +0300

    update tags

--- theme/components/tag.ts
+++ theme/components/tag.ts
@@ -7,7 +7,7 @@ export default {
       const darkBg = transparentize(`${c}.200`, 0.16)(theme);
       return {
         container: {
-          bg: mode(`${c}.300`, darkBg)(props),
+          bg: mode(`${c}.950`, darkBg)(props),
           color: mode(`${c}.900`, `${c}.200`)(props),
         },
       };
@@ -16,11 +16,11 @@ export default {
   sizes: {
     md: {
       container: {
-        fontSize: "cap2",
-        lineHeight: "cap2",
-        borderRadius: "base",
+        fontSize: "body2",
+        lineHeight: "body2",
+        borderRadius: "4px",
         px: 2,
-        py: 1,
+        py: 1.5,
       },
     },
   },
